497ff202ccfc2fda2658fd7ce19a3675252a5629
tobyy.ger <tobiasger@seznam.cz>
2026-01-12 21:40:36 +0100

    Update projects list with new registration form

--- projects-data.php
+++ projects-data.php
@@ -1,14 +1,21 @@
-<?php $projects = [
-    [
-        "link" => "penzForm.php",
-        "title" => "Rezervační formulář.",
-        "desc"  => "Můj první školní <b>PHP</b> projekt, rezervační formulář pro smyšlený penzion",
-        "img"   => "logopenz.png"
-    ],
+<?php
+$projects = [
     [
         "link" => "conv_intro.html",
         "title" => "Měnový konvertor",
         "desc"  => "Závěrečný školní projekt druhého ročníku.",
         "img"   => "logoconv.png"
+    ],
+    [
+        "link" => "https://registrace.endora.site",
+        "title" => "Registrační formulář",
+        "desc"  => "Závěrečný školní projekt druhého pololetí třetího ročníku, registrační formulář pro smyšlené akce.",
+        "img"   => "logoakce.png"
+    ],
+    [
+        "link" => "penzForm.php",
+        "title" => "Rezervační formulář",
+        "desc"  => "Můj první školní <b>PHP</b> projekt, rezervační formulář pro smyšlený penzion. (bez CSS)",
+        "img"   => "logopenz.png"
     ]
 ];
